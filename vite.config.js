901ee8fd452f5851ae5869844e3f142cab7188a2
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2026-05-28 04:12:14 +0530

    chore: try fixing asset path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import litCss from "vite-plugin-lit-css";
 
 export default defineConfig({
-  base: "/wc-wysiwyg-editor-carbon/",
+  base: "./",
   plugins: [litCss()],
 });
